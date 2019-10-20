6cf96b0facc491c4ab2759fb5fd0cd868e4719e6
Sanjay Sachdev <sanjaypsachdev@gmail.com>
2019-10-20 20:47:13 +0530

    Added text cases for the auth reducer

--- src/store/reducers/auth.test.js
+++ src/store/reducers/auth.test.js
@@ -14,6 +14,16 @@ describe('auth reducer', () => {
     expect(reducer(undefined, {})).toEqual(initialState);
   });
 
+  it('should return a loading status while starting the login process', () => {
+    expect(reducer(initialState, {
+      type: actionTypes.AUTH_START
+    })).toEqual({
+      ...initialState,
+      error: false,
+      loading: true
+    })
+  });
+
   it('should store the token upon login', () => {
     expect(reducer(initialState, {
       type: actionTypes.AUTH_SUCCESS,
@@ -40,5 +50,37 @@ describe('auth reducer', () => {
       loading: false,
       authRedirectPath: '/'
     });
+  });
+
+  it('should clear the token upon logout', () => {
+    expect(reducer({
+      ...initialState,
+      token: 'some-token',
+      userId: 'some-user-id'
+    }, {
+      type: actionTypes.AUTH_LOGOUT
+    })).toEqual(initialState);
+  });
+
+  it('should report error upon login failure', () => {
+    const error = 'Invalid Username / Password';
+    expect(reducer(initialState, {
+      type: actionTypes.AUTH_FAIL,
+      error
+    })).toEqual({
+      ...initialState,
+      error
+    });
+  });
+
+  it('should be able to redirect to the path last accessed before trying to log in, upon successful login', () => {
+    const path = '/checkout';
+    expect(reducer(initialState, {
+      type: actionTypes.SET_AUTH_REDIRECT_PATH,
+      path
+    })).toEqual({
+      ...initialState,
+      authRedirectPath: path
+    })
   });
 });
