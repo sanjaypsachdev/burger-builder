64a1561b1c72229ee21f19dfc3ce1d7645577731
sanjaypsachdev <sanjaypsachdev@gmail.com>
2020-06-13 11:29:30 +0530

    Corrected the invalid import
    Corrected the invalid import

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -5,7 +5,7 @@ import Order from '../../components/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
-import Spinner from '../../components//UI/Spinner/Spinner';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 export class Orders extends Component {
 
